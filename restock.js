d3714e3a2d579d75e54d30552185c6737196b403
Baron Von Clop <baronvonclop@gmail.com>
2017-11-18 01:33:40 -0500

    Beginning to set assembly script.

--- restock.js
+++ restock.js
@@ -1,4 +1,45 @@
-var storeChoice = "";
+var storeChoice = "1";
 var assembleArray = [];
-assembleArray[1] = "// ==UserScript==";
-assembleArray[2] = "// @name         Restocking Profit Highlighter (" + storeChoice + ")";
+var greenArray = [];
+var yellowArray = [];
+var redArray = [];
+var blankArray =[];
+
+function assembleScript(){
+    var greenText = $('#greenBox').val();
+
+    assembleArray.push("// ==UserScript==");
+    assembleArray.push("// @name         Restocking Profit Highlighter (" + storeChoice + ")");
+    assembleArray.push("// @namespace    http://neopets.lipomancer.com/")
+    assembleArray.push("// @version      1.0");
+    assembleArray.push("// @description  Automatically highlights profitable items in Neopets stores.");
+    assembleArray.push("// @author       MediaTriggerWords, modified by Lipomancer");
+    assembleArray.push("// @match        http://www.neopets.com/objects.phtml?obj_type=" + storeChoice + "&type=shop");
+    assembleArray.push("// ==/UserScript==");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+    assembleArray.push("");
+
+
+    //Debug -- sends every line to the console.
+    for (var i = 0; i < assembleArray.length; i++) {
+        console.log(assembleArray[i]);
+    }
+}
